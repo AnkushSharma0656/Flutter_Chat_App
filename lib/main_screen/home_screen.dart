fba852583a7a0ecc53d56bd740093a16c28f286c
AnkushSharma0656 <ankushsharma0656@gmail.com>
2024-07-06 14:43:07 +0530

    chats screen

--- lib/main_screen/home_screen.dart
+++ lib/main_screen/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:chatty/main_screen/chats_list_screen.dart';
+import 'package:chatty/main_screen/groups_screen.dart';
+import 'package:chatty/main_screen/people_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:chatty/utilities/assets_manager.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,17 +14,52 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
+  int currentIndex = 0;
+  final PageController pageController = PageController(initialPage: 0);
+  final List<Widget> pages = [
+    ChatsListScreen(),
+    GroupsScreen(),
+    PeopleScreen()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
          appBar: AppBar(
            title: const Text("Chatty Chat"),
-           actions: [
-             CircleAvatar()
+           actions: const [
+             Padding(
+               padding: EdgeInsets.all(8.0),
+               child: CircleAvatar(
+                 radius: 20,
+                 backgroundImage: AssetImage(AssetsManager.userImage),
+               ),
+             )
            ],
          ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (index){
+        setState(() {
+          currentIndex = index;
+        });
+        },
+        children: pages,
+
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.chat_bubble_2),label: 'Chats'),
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.group),label: 'Groups'),
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.globe),label: 'People'),
+      ],
+        currentIndex: currentIndex,
+        onTap: (index){
+        pageController.animateToPage(index, duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
+        setState(() {
+          currentIndex = index;
+        });
+        },
+      ),
 
     );
   }
